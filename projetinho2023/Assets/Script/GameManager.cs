550ebe1dda21d1d8353b97fd502b299514cde768
José Lucas de Morais Soares <morais.j@academico.ifrn.edu.br>
2023-04-03 18:00:55 -0300

    aula 03/04/2023

---
+++ projetinho2023/Assets/Script/GameManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(transform);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        LoadScene("MainMenu");
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    
+}
